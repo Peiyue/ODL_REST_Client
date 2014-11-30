73772cb37e66405a80602132184cd8984003c33a
Peiyue <peiyuekth@gmail.com>
2014-11-30 14:52:33 +0100

    TBA

---
+++ balancerBuilder.py
@@ -0,0 +1,4 @@
+from flow_finder import flow_finder
+def balancerBuilder():
+    Balancer={'switchId_1','switchId_2',rules[[flow_finder('flow9'),flow_finder('flow9')],[flow_finder('flow9'),flow_finder('flow9')]}
+    return Balancer
--- lbt.py
+++
@@ -1,2 +0,0 @@
-Balancer={'switchId_1','switchId_2',rules[[flow_finder('flow9'),flow_finder('flow9')],[flow_finder('flow9'),flow_finder('flow9')]}
-

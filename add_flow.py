399ece5ee07e9d7e2f9ba0dcfa0461a296309c30
Peiyue <peiyuekth@gmail.com>
2014-11-30 10:06:00 +0100

    TBD

--- add_flow.py
+++ add_flow.py
@@ -1,12 +1,10 @@
 import requests
 import json
 from requests.auth import HTTPBasicAuth
+from conf import conf
 def add_flow(flow):
-	#Connection confi
-	controllerIp = 'http://127.0.0.1:8080'
-	ODL_user='admin'
-	ODL_passwd = 'admin'
-	auth = HTTPBasicAuth(ODL_user,ODL_passwd)
+	#get connection info
+        conf=conf()	
 	#Json confi
 	data ={"installInHw":"true", "name":'', "node": {"id":"", "type":"OF"}, "ingressPort":"2", "etherType": "0x800", "protocol": "6", "tpDst": "80", "priority":"65535","actions":[""]}
         data['name']=flow['flowName']
@@ -16,9 +14,10 @@ def add_flow(flow):
 	
 	headers = {'Content-type': 'application/json'}
 	flowUrl = '/controller/nb/v2/flowprogrammer/default/node/OF/'+flow['switchId']+'/staticFlow/'+flow['flowName']
-	url = controllerIp + flowUrl
+	url =conf['controllerIp'] + flowUrl
 	#Put flow
-	result=requests.put(url,auth=auth,headers=headers,data=json.dumps(data))
+	result=requests.put(url,auth=auth=conf['auth'],headers=headers,data=json.dumps(data))
+        #Print result
 	print result
 #Reference:https://jenkins.opendaylight.org/controller/job/controlller-merge-hydrogen-stable/lastSuccessfulBuild/artifact/opendaylight/northbound/flowprogrammer/target/site/wsdocs/resource_FlowProgrammerNorthbound.html
 #Reference:http://net-ed.blogspot.se/2013/11/using-python-rest-api-to-manage-flow.html

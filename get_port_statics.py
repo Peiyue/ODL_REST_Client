4505a4eacc8a3c27a86df3a848ad95e0fb60bb7c
Peiyue <peiyuekth@gmail.com>
2014-11-30 10:15:13 +0100

    tba

--- get_port_statics.py
+++ get_port_statics.py
@@ -11,5 +11,7 @@ url = conf['controllerIp'] + flowUrl
 #get flow static
 result=requests.get(url,auth=conf['auth'],headers=headers)
 print result
+data=result.json()
+print data
 #Reference:https://jenkins.opendaylight.org/controller/job/controlller-merge-hydrogen-stable/lastSuccessfulBuild/artifact/opendaylight/northbound/flowprogrammer/target/site/wsdocs/resource_FlowProgrammerNorthbound.html
 #Reference:http://net-ed.blogspot.se/2013/11/using-python-rest-api-to-manage-flow.html

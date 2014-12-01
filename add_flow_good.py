f1d1374e3282fc36cf981ce38090f2e76bbca43b
Peiyue <peiyuekth@gmail.com>
2014-12-01 20:07:24 +0100

    TBA

--- add_flow_good.py
+++ add_flow_good.py
@@ -4,18 +4,23 @@ from requests.auth import HTTPBasicAuth
 from conf import conf
 def add_flow(flow):
 	#get connection info
-        conf=conf()
-        #Json confi
+        controllerIp='http://127.0.0.1:8080'
+        ODL_user='admin'
+        ODL_password='admin'
+        auth = HTTPBasicAuth(ODL_user,ODL_password)
+                   
+	#Json confi
 	data ={"installInHw":"true", "name":'', "node": {"id":"", "type":"OF"}, "ingressPort":"2", "etherType": "0x800", "protocol": "6", "tpDst": "80", "priority":"65535", "actions":[""]}
         data['name']=flow['flowName']
         data['node']['id']=flow['switchId']
         data['ingressPort']=flow['inComingPort']
 	data['actions'][0]=flow['actions']
+	
 	headers = {'Content-type': 'application/json'}
 	flowUrl = '/controller/nb/v2/flowprogrammer/default/node/OF/'+flow['switchId']+'/staticFlow/'+flow['flowName']
-	url =conf['controllerIp']+flowUrl
+	url =controllerIp+flowUrl
 	#Put flow
-	result=requests.put(url,auth=conf['auth'],headers=headers,data=json.dumps(data))
+	result=requests.put(url,auth=auth,headers=headers,data=json.dumps(data))
         #Print result
 	print result
 #Reference:https://jenkins.opendaylight.org/controller/job/controlller-merge-hydrogen-stable/lastSuccessfulBuild/artifact/opendaylight/northbound/flowprogrammer/target/site/wsdocs/resource_FlowProgrammerNorthbound.html

1df6e266f87595c53908faa66b77f24ee1a13d17
Peiyue <peiyuekth@gmail.com>
2014-12-03 11:36:54 +0100

    TBA

--- extract_flow.py
+++ extract_flow.py
@@ -15,8 +15,8 @@ def extract_flow(data):
     flow['switchId']=temp[1]
     flow['inComingPort']=temp[2]
     flow['actions'][0]=temp[3]
-    if len(temp)>4
+    if len(temp)>4:
         i=4
-        for i in range(len(temp))
+        for i in range(len(temp)):
             flow['status'].append[temp[i]]
     return flow

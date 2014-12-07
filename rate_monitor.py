a3579ae1d6adf66c933c0c2d7d1585ca5bf2993b
Peiyue <peiyuekth@gmail.com>
2014-12-07 16:40:25 +0100

    tba

--- rate_monitor.py
+++ rate_monitor.py
@@ -4,17 +4,18 @@ from check_old_switches import check_old_switches
 from check_new_switches import check_new_switches
 from check_ports_rate import check_ports_rate
 
-data_old=get_all_ports_statics()
+time_interval=3
+
 print '==========================================================='
 print 'Rate Moniting Started...'
-print '===================Datarate Record==================='
 
+data_old=get_all_ports_statics()
 while 1:
-	time_interval=3
 	time.sleep(time_interval)
 	data_new=get_all_ports_statics()
 	result_switch={'Added Port':[],'Deleted Port':[]}
 	#check_old_ports(data_old,data_new,result,time_interval)
+        print '==========================================================='
         result_switch=check_ports_rate(data_old,data_new,result_switch,time_interval)
         data_old=data_new  
 	
--- test-sleep.py
+++ test-sleep.py
@@ -2,3 +2,8 @@ import time
 print 'start'
 time.sleep(3)
 print 'end'
+
+test=['1','2','3','4','5','6']
+l=len(test)
+for i in range(l):
+ print test[i]

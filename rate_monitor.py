54938126832f1e037b46942403698731303474da
Peiyue <peiyuekth@gmail.com>
2014-12-07 15:24:00 +0100

    tba

--- rate_monitor.py
+++ rate_monitor.py
@@ -15,8 +15,8 @@ while 1:
 	data_new=get_all_ports_statics()
 	result_switch={'Added Switch:':[],'Deleted Switch:':[]}
 	#check_old_ports(data_old,data_new,result,time_interval)
-    result_switch=check_old_ports(data_old,data_new,result_switch,time_interval)
-    data_old=data_new  
+        result_switch=check_old_ports(data_old,data_new,result_switch,time_interval)
+        data_old=data_new  
 	
 	
 

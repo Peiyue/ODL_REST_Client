fafda696e5e982dd400de025fcf91fa579db5f90
Peiyue <peiyuekth@gmail.com>
2014-12-07 12:23:12 +0100

    tba

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ while 1:
     data_old=get_all_ports_statics()
     data_new=get_all_ports_statics()
     result_switch={'Added Switch:':[],'Deleted Switch:':[]}
-    check_old_switches(data_old,data_new)
-    check_new_switches(data_old,data_new)
+    check_old_switches(data_old,data_new,result_switch)
+    check_new_switches(data_old,data_new,result_switch)
     print result_switch
 
 

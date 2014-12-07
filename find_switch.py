c1c12e7dcbe90651a839941344b17c54ffef36c0
Peiyue <peiyuekth@gmail.com>
2014-12-07 15:48:56 +0100

    TBA

--- find_switch.py
+++ find_switch.py
@@ -2,7 +2,7 @@ def find_switch(switchid,data_new):
 		numofswitchs_old=len(data['portStatistics'])
 		for switchindex in rang(numofswitchs_old):
                     flag=0
-                        if switchid=data_new['portStatistics'][switchindex]['node']['id']
+                        if switchid=data_new['portStatistics'][switchindex]['node']['id']:
                             return switchindex
                             flag=1
 			    break

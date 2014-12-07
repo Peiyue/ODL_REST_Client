6136582ebf10ea3496d5d6f1279534ce3319dfc0
Peiyue <peiyuekth@gmail.com>
2014-12-07 16:29:02 +0100

    tba

--- datarate_calculator.py
+++ datarate_calculator.py
@@ -1,9 +1,9 @@
 def datarate_calculator(Byte_old,Byte_new,time_interval):
 	rate=(float(Byte_new)-float(Byte_old))/time_interval
-	if rate>100
+	if rate>100:
 		rate=rate/1024
 		datarate=str(rate)+'Mb/second'
-	else
+	else:
 		datarate=str(rate)+'Byte/second'
 	return datarate
 

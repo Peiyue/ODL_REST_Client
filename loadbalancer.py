56e11554180f01f38b85e5ee99b70a6c6388dcb4
Peiyue <peiyuekth@gmail.com>
2014-12-09 21:21:25 +0100

    TBA

--- loadbalancer.py
+++ loadbalancer.py
@@ -7,8 +7,8 @@ from flow_switch import flwo_switch
 from balancerBuilder import balancerBuilder
 from flow_finder import flow_finder
 
-def loadbalancer(datarate,Balancer)
-        #Balancer['balancerName']
+def loadbalancer(datarate,Balancer):
+	#Balancer['balancerName']
 	#Balancer['switchId']
 	#Balancer['portId']
 	#Balancer['threshhold']
@@ -18,17 +18,17 @@ def loadbalancer(datarate,Balancer)
 	flow_2=flow_finder(Balancer['flows'][1])
 
         
-	elif  Balancer['type']='min':
-		if datarate<Balancer['threshhold']:
-			if flow_1['status']==0:
-				switch_flow(flow_1)
-				switch_flow(flow_2)
-				print 'Loadbalancer--'+Balancer['balancerName']+'--trigger'
+        if  Balancer['type']=='min':
+			if datarate<Balancer['threshhold']:
+				if flow_1['status']==0:
+					switch_flow(flow_1)
+					switch_flow(flow_2)
+					print 'Loadbalancer--'+Balancer['balancerName']+'--trigger'
 			#else:
 			#	switch_flow(flow_2)
 			#	switch_flow(flow_1)
 
-	elif  Balancer['type']='max':
+	elif  Balancer['type']=='max':
 		if datarate>Balancer['threshhold']:
 			#if flow_1['status']==0:
 				switch_flow(flow_1)

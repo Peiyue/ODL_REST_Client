7361817afa0c035b69ba69ecebdd642eeba8fb33
Peiyue <peiyuekth@gmail.com>
2014-12-11 17:10:25 +0100

    tba

--- check_topo.py
+++ check_topo.py
@@ -1,5 +1,5 @@
 def check_topo(data_new,data_old,result_topo_deleted):
-    num_of_old_links=len(data_old['edgeProperties'])
+    #num_of_old_links=len(data_old['edgeProperties'])
     num_of_new_links=len(data_new['edgeProperties'])
     for link_index_old in range(num_of_old_links):
         switch_id_old=data_old['edgeProperties'][link_index_old]['edge']['headNodeConnector']['node']['id']

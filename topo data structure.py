f9ace54aefb2f3a4df4b97b9be8c5964fedca008
Peiyue <peiyuekth@gmail.com>
2014-12-11 16:32:12 +0100

    TBA

---
+++ topo data structure.py
@@ -0,0 +1,68 @@
+data={
+	"edgeProperties":[
+						{
+					"edge":{
+			"tailNodeConnector":{
+							"node":{
+									"id":"00:00:00:00:00:00:00:02",
+									"type":"OF"
+									},
+							"id":"2",
+							"type":"OF"
+								},
+			"headNodeConnector":{
+							"node":{
+									"id":"00:00:00:00:00:00:00:51",
+									"type":"OF"
+									},
+							"id":"2",
+							"type":"OF"
+								}
+							},
+			"properties":{
+						"timeStamp": {
+										"value": 1379527162648,
+										"name": "creation",
+									},
+							"name": {
+									"value": "s2-eth3"
+									},
+							"state": {
+										"value": 1
+									},
+							"config": {
+										"value": 1
+										},
+							"bandwidth": {
+											"value": 10000000000
+											}
+						}
+						},
+{
+"edge":{
+"tailNodeConnector":{
+"node":{
+"id":"00:00:00:00:00:00:00:51",
+"type":"OF"
+},
+"id":"2",
+"type":"OF"
+},
+"headNodeConnector":{
+"node":{
+"id":"00:00:00:00:00:00:00:02",
+"type":"OF"
+},
+"id":"2",
+"type":"OF"
+}
+},
+"properties":{
+"timeStamp": {
+"value": 1379527162648,
+"name": "creation",
+}
+}
+}
+]
+}
---
+++ topo_monitor.py
@@ -0,0 +1,38 @@
+import time
+from time import ctime
+from check_topo import check_topo
+from check_topo import check_topo
+from get_topology import get_topology
+from check_topo import check_topo
+from loadbalancer import loadbalancer
+
+
+time_interval=0
+
+print '==========================================================='
+print ctime(),'Topology Monitor started'
+print '========================Log================================'
+
+data_old=get_topology()
+
+
+numoflinks=len(data['edgeProperties'])
+
+print str(numoflinks),'links detected'
+
+for link_index_old in range(numoflinks):
+    s1=data['edgeProperties'][link_index_old]['edge']['tailNodeConnector']['node']['id'] #switch ID
+    p1=data['edgeProperties'][link_index_old]['edge']['tailNodeConnector']['id'] #Port ID
+    s2=data['edgeProperties'][link_index_old]['edge']['headNodeConnector']['node']['id'] #switch ID
+    p2=data['edgeProperties'][link_index_old]['edge']['headNodeConnector']['id'] #Port ID
+    print' Switch ',s1,' port ',p1,' connected to ',' Switch ',s2,' port ',p2
+
+while 1:
+	time.sleep(time_interval)
+	data_new=get_topology
+	result_topo_deleted={'headNodeConnector':[],'hn port':[],'tailNodeConnector':[],'tn port':[]}
+	result_topo_added={'headNodeConnector':[],'hn port':[],'tailNodeConnector':[],'tn port':[]}
+        result_switch=check_topo(data_old,data_new,result_topo_added,result_topo_deleted)
+        data_old=data_new
+
+#print result_switch

0c5400f1c2324ea66c8dea5cf0f7326fc0f0cc1c
Peiyue <peiyuekth@gmail.com>
2014-12-12 00:18:07 +0100

    tba

--- redundancy_manager.py
+++ redundancy_manager.py
@@ -11,11 +11,11 @@ def redundancy_manager(tailNodeConnector,headNodeConnector,reds):
 
     #search if any redundancy rules hit
     for red in reds:
-        if tailNodeConnector=red['tailNodeConnector']
-            if headNodeConnector=red['headNodeConnector']
+        if tailNodeConnector==red['tailNodeConnector']:
+            if headNodeConnector==red['headNodeConnector']:
                 print 'redundancy activated:',red['redundancy name']
-                for flow_add in red['Add_flows']
+                for flow_add in red['Add_flows']:
                     add_flow(flow_add)
-                for flow_delete in red['Delete flows']
+                for flow_delete in red['Delete flows']:
                     delete_flow(flow_delete)
     

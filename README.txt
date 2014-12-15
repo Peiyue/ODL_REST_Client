2ced991b54efe18dfcc054a5b650e38701fbd169
Peiyue <peiyuekth@gmail.com>
2014-12-15 20:13:27 +0100

    tba

--- README.txt
+++ README.txt
@@ -3,14 +3,25 @@ ODL_REST_Client
 ===Install===
 git clone git://github.com/Peiyue/ODL_REST_Client
 ===Configure===
-1.Open conf.py to set the IP,Username,Password of your OpenDaylight
-2.Open flowtable.text to see up the flows
-===Run===
-#add flow from flowtable.txt to the ODL
-python flow_adder.py
-#swtich monitor
-python switch_monitor.py
-#get the datarate of all the ports
-python rate_monitor.py
-#a demo program to show get the statistics of a port
-python get_port_statics_demo.py
+Open conf.py to set the IP,Username,Password of your OpenDaylight
+===Functions===
+1)flow_adder.py
+  add flow from flowtable.txt to the ODL
+  use the flowtable.txt to set the flows
+2)swtich_monitor.py
+  moniting the status of the swtiches(added or removed )
+3)rate_monitor.py
+  get the datarate of the ports
+  Usage: python rate_monitor.py [-m mode] [-t time]
+  argument -t time the time interval of rate monitor in seconds,default value is 3
+	 -m mode set the mode
+       	         'all' print monitor all the ports,default
+                 'customer' monitor the rate of ports specified in ratetable.txt
+4)loadbalancer.py
+  loadbalancer function
+  use the loadbalancertalbe.txt to configure
+5)topo_monitor.py
+  link redundancy
+  print the topology
+  print info when link changes
+
--- test-sleep.py
+++ test-sleep.py
@@ -1,9 +1,133 @@
-import time
-print 'start'
-time.sleep(3)
-print 'end'
+import Tkinter
+from flow_adder import flow_adder
+from flow_deleter import flow_deleter
+import thread
+import tkMessageBox
 
-test=['1','2','3','4','5','6']
-l=len(test)
-for i in range(l):
- print test[i]
+
+
+#functions
+def delete_action():
+    thread.start_new_thread(flow_deleter,())
+def add_action():
+    thread.start_new_thread(flow_adder,())
+def about():
+    tkMessageBox.showinfo("About", "Opendaylight REST client, 2014")
+#the main window
+
+top=Tkinter.Tk(className='Opendaylight RRST client')
+
+#flow frame
+frame_flow = Tkinter.LabelFrame(top,text="Basic Flow functions")
+frame_flow.pack()
+
+button = Tkinter.Button(frame_flow,width=30,padx=5,pady=2)
+button['text'] = 'Add default flows'
+button['command'] =add_action
+button['relief']='groove'
+button.pack(side=Tkinter.LEFT)
+
+button = Tkinter.Button(frame_flow,width=30,padx=5,pady=2)
+button['text'] = 'Delete all default flows'
+button['command'] =delete_action
+button['relief']='groove'
+button.pack(side=Tkinter.LEFT)
+
+
+
+
+
+
+function_flow = Tkinter.LabelFrame(top,text="Network Functions")
+function_flow.pack()
+
+button = Tkinter.Button(function_flow,width=30,padx=5,pady=2)
+button['text'] = 'Redundancy/'
+button['command'] =add_action
+button['relief']='groove'
+button.pack(side=Tkinter.LEFT)
+
+button = Tkinter.Button(function_flow,width=30,padx=5,pady=2)
+button['text'] = 'Loadbalancer'
+button['command'] =delete_action
+button['relief']='groove'
+button.pack(side=Tkinter.LEFT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+L1 = Tkinter.Label(top, text="Flow Name")
+L1.pack( side = Tkinter.LEFT)
+E1 = Tkinter.Entry(top, bd =5)
+E1.pack(side = Tkinter.LEFT)
+
+
+L2 = Tkinter.Label(top, text="Switch ID")
+L2.pack( side = Tkinter.LEFT)
+E2 = Tkinter.Entry(top, bd =5)
+E2.pack(side = Tkinter.LEFT)
+
+
+
+L3 = Tkinter.Label(top, text="Incoming port")
+L3.pack( side = Tkinter.LEFT)
+E3 = Tkinter.Entry(top, bd =5)
+E3.pack(side = Tkinter.LEFT)
+
+
+L4 = Tkinter.Label(top, text="Actions")
+L4.pack( side = Tkinter.LEFT)
+E4 = Tkinter.Entry(top, bd =5)
+E4.pack(side = Tkinter.LEFT)
+
+button = Tkinter.Button(top,padx=5,pady=2)
+button['text'] = 'Add flow'
+button['command'] =add_action
+button['relief']='groove'
+button.pack(side=Tkinter.LEFT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+menubar = Tkinter.Menu(top)
+filemenu = Tkinter.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Close", command=top.quit)
+menubar.add_cascade(label="System", menu=filemenu)
+
+
+helpmenu = Tkinter.Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About",command=about)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+top.config(menu=menubar)
+
+Tkinter.mainloop()

11e81da516b43d045acfe969b2c43d3975da3f3c
Peiyue <peiyuekth@gmail.com>
2014-12-20 18:39:28 +0100

    tba

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+# This is a very simple example
+cd ..
+sudo rm -rf ODL_REST_Client
+sudo git clone git://github.com/Peiyue/ODL_REST_Client
---
+++ pingall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+# This is a very simple example
+ping 100.0.0.1 -c 5
+ping 100.0.0.2 -c 5
+ping 100.0.0.3 -c 5
+ping 100.0.0.4 -c 5
+ping 200.0.0.1 -c 5
+ping 200.0.0.2 -c 5
+ping 200.0.0.3 -c 5
+ping 200.0.0.4 -c 5
